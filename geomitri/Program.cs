4d67a04f3c025d8ca11ae6bfd3d0677ce0f7f664
Emillarsen194 <47022984+Emillarsen194@users.noreply.github.com>
2019-03-18 12:10:08 +0100

    Add project files.

---
+++ geomitri/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geomitri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square foursites = new Square(200); //making new object square
+
+            Square foursitestwo = new Square(300);
+
+            Square empty = new Square();
+
+            empty.Side = 20;
+            Console.WriteLine(foursites.Omkreds());
+
+            Console.WriteLine(foursitestwo.Omkreds());
+
+            Console.WriteLine(foursites.Areal());
+
+            Console.WriteLine(empty.Areal());
+        }
+    }
+}
---
+++ geomitri/Square.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geomitri
+{
+    class Square
+    {
+        private double side;
+
+            public Square(double side)
+        {
+            this.side = side;
+        }
+
+        public double Side
+        {
+            get
+            {
+                return side;
+            }
+            set
+            {
+                side = value;
+            }
+        }
+
+        public Square() // constructor we can use for logic 
+        {
+            
+        }
+
+        public double Omkreds() //finds omkreds
+        {
+           return side * 4;
+        }
+
+        public double Areal() //finds areal
+        {
+            return Math.Pow(side, 2);
+        }
+    }
+}
